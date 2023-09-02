c0f5aa0f52a9aaa2d8f863437660af1ac522f308
Azharuddinn <Azharuddinkhann1@gmail.com>
2023-09-02 15:45:51 +0500

    CRUD on users created

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,7 @@ const pool = require('../db/connect_to_db')
 productsRouter.get('/' , (req, res) => {
     pool.query('SELECT * FROM products', (error, results) => {
         if (error) {
-          res.status(500).json({ error: 'Internal Server Error' });
+          return res.status(500).json({ error: 'Internal Server Error' });
         }
         res.status(200).json(results.rows)
     })
@@ -20,7 +20,7 @@ productsRouter.get('/:id' , (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
       }
       if (results.rowCount === 0) {
-        res.status(404).json({ message: 'Product not found' });
+        return res.status(404).json({ message: 'Product not found' });
       }
       res.status(200).json(results.rows)
   })
@@ -35,7 +35,7 @@ productsRouter.get('/category/:category' , (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
       }
       if (results.rows.length === 0){
-        res.status(404).send('No products found for the specified category');
+        return res.status(404).send('No products found for the specified category');
       }
       res.status(200).json(results.rows)
   })
@@ -47,7 +47,7 @@ productsRouter.post('/' , (req, res) => {
 
   pool.query('INSERT INTO products (brand,name,category,price,image) VALUES ($1, $2, $3, $4, $5)', [brand,name,category,gender,price,image], (error, results) => {
       if (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
       }
       res.status(200).json('product successfully added')
   })
@@ -61,11 +61,11 @@ productsRouter.put('/:id' , (req, res) => {
 
   pool.query(`UPDATE products SET ${column} = $1 WHERE id = $2`,[newValue, id], (error, results) => {
       if (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
       }
 
       if (results.rowCount === 0) {
-        res.status(404).json({ message: 'Product not found' });
+        return res.status(404).json({ message: 'Product not found' });
       }
       res.status(200).json(` product's ${column} updated successfully`)
   })
@@ -78,11 +78,11 @@ productsRouter.delete('/:id' , (req, res) => {
 
   pool.query(`delete from products where id = $1`,[id], (error, results) => {
       if (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
       }
 
       if (results.rowCount === 0) {
-        res.status(404).json({ message: 'Product not found' });
+        return res.status(404).json({ message: 'Product not found' });
       }
 
       res.status(200).json({message:'product deleted successfully'})
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const usersRouter = express.Router();
 const pool = require('../db/connect_to_db');
 
 usersRouter.get('/' , (req, res) => {
+
     pool.query('SELECT * FROM users', (error, results) => {
+
         if (error) {
           res.status(500).json({ error: 'Internal Server Error' });
         }
@@ -11,6 +13,73 @@ usersRouter.get('/' , (req, res) => {
     })
 });
 
+usersRouter.get('/:id' , (req, res) => {
 
+    const id  = req.params.id;
+
+    pool.query('SELECT * FROM users where id = $1', [id], (error, results) => {
+
+        if (error) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        if (results.rowCount === 0) {
+            return res.status(404).send( 'user not found' );
+        }
+
+        res.status(200).json(results.rows)
+    })
+});
+
+usersRouter.post('/' , (req, res) => {
+
+    const {username,password} = req.body;
+
+    pool.query('insert into users (username,password) values($1,$2)', [username,password], (error, results) => {
+
+        if (error) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res.status(200).send('user successsfully added')
+    })
+});
+
+usersRouter.put('/:id' , (req, res) => {
+
+    const id  = req.params.id;
+    const column = Object.keys(req.body)[0];
+    const newValue = req.body[column];
+
+    pool.query(`UPDATE users SET ${column} = $1 WHERE id = $2`, [newValue,id], (error, results) => {
+
+        if (error) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        if (results.rowCount === 0) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).send(`users's ${column} updated successfully`)
+    })
+});
+
+usersRouter.delete('/:id' , (req, res) => {
+
+    const id  = req.params.id;
+
+    pool.query(`delete from users where id = $1`,[id],  (error, results) => {
+
+        if (error) {
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        if (results.rowCount === 0) {
+            return res.status(404).send( 'user not found' );
+        }
+
+        res.status(200).send('user deleted successfully')
+    })
+});
 
 module.exports = usersRouter;
